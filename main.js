35b9a7713966bf692c2aea7f147c9633101dd585
Dhanu9002 <87596359+Dhanu9002@users.noreply.github.com>
2022-10-10 16:43:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+nose_x = 0;
+nose_y = 0;
+
+function preLoad(){
+    clown_nose = loadImage('https://i.postimg.cc/05c0fj22/clownnose.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(nose_x,nose_y,20);
+    image(clown_nose,nose_x,nose_y,30,30);
+}
+
+function take_snapshot(){
+    save("Picture.png");
+}
